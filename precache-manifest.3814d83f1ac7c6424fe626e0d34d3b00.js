2007eeade8171b935719a44d1946118801a0046f
Lukasz Winowicz <winowiczlukasz@gmail.com>
2019-06-21 20:54:00 +0200

    Updates

---
+++ precache-manifest.3814d83f1ac7c6424fe626e0d34d3b00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9345111bf74cb7e19d9b333551012d0b",
+    "url": "/VideoPlayer-yt-api/index.html"
+  },
+  {
+    "revision": "43d8b67888e0f964f563",
+    "url": "/VideoPlayer-yt-api/static/css/main.b7e903a4.chunk.css"
+  },
+  {
+    "revision": "7e6ac1444ef9242ebc00",
+    "url": "/VideoPlayer-yt-api/static/js/2.824d5390.chunk.js"
+  },
+  {
+    "revision": "43d8b67888e0f964f563",
+    "url": "/VideoPlayer-yt-api/static/js/main.fa59e07f.chunk.js"
+  },
+  {
+    "revision": "fab7b7eea622996a0bad",
+    "url": "/VideoPlayer-yt-api/static/js/runtime~main.4ba0de58.js"
+  }
+]);
